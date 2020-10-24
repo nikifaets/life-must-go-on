ef0ed25066b2dc4fc4758c4e6588ad27a0a76ab1
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-10-24 18:01:38 +0300

    list only names

--- backend/file.js
+++ backend/file.js
@@ -1,14 +1,19 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://life-must-go-on:KamenNikiDimo@cluster0.bf5oa.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
+var dbNames = [];
 client.connect(err => {
 	if(err) throw err;
 	db = client.db("events");
 	db.listCollections().toArray(function(err, collInfos) {
 		// collInfos is an array of collection info objects that look like:
 		// { name: 'test', options: {} }
-		console.log(collInfos);
+		for( x of collInfos){
+			dbNames.push(x.name);
+		}
+		console.log(dbNames);
 	});
 	client.close();	
 });
+
 
